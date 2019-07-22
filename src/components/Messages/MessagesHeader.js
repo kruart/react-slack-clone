ceac22333ce7efcea80c7fa90c14fbfdec5309d8
arthur.krukowski <weoz@ukr.net>
2019-07-22 17:41:53 +0300

    add ability to search messages

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -4,7 +4,7 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 class MessagesHeader extends Component {
 
     render() {
-        const { channelName, numUniqueUsers } = this.props;
+        const { channelName, numUniqueUsers, handleSearchChange, searchLoading } = this.props;
 
         return (
             <Segment clearing>
@@ -20,10 +20,13 @@ class MessagesHeader extends Component {
 
                 {/*Channel Search Input*/}
                 <Header floated="right">
-                    <Input size="mini"
-                           icon="search"
-                           name="searchTerm"
-                           placeholder="Search Messages"/>
+                    <Input
+                        loading={searchLoading}
+                        onChange={handleSearchChange}
+                        size="mini"
+                        icon="search"
+                        name="searchTerm"
+                        placeholder="Search Messages"/>
                 </Header>
             </Segment>
         );
